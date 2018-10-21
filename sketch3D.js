cade36362139f78be274a01497b8af9e0521948b
GonzaloFernandez <fernandez.gfg@gmail.com>
2018-10-21 15:00:42 -0300

    Agrego imagenes que faltaban

--- sketch3D.js
+++ sketch3D.js
@@ -1,6 +1,14 @@
 var satModel;
 
 var img;
+var img_sea;
+var img_snow;
+var img_cloud;
+var img_tc;
+var img_od;
+var img_dt;
+var img_nt;
+
 var angleX;
 var angleY;
 var r = 200;   // earth radium
@@ -14,6 +22,10 @@ function preload(){
     img_sea = loadImage("assets/sea.jpg");
     img_snow = loadImage("assets/snow.jpg");
     img_cloud = loadImage("assets/clouds.jpg");
+    img_tc = loadImage("assets/true_color_oct2018_lower_res.jpg");
+    img_od = loadImage("assets/ocean_depth2002_lower_res.jpg");
+    img_dt = loadImage("assets/land_temperature_day_feb2017.jpg");
+    img_nt = loadImage("assets/land_temperature_night_feb2017.jpg");
     satModel = loadModel("satellite_obj.obj");
 }
 
@@ -27,14 +39,20 @@ var b_cloud;
 var b_sea;
 var b_snow;
 var b_clean;
-
+var b_tc;
+var b_od;
+var b_dt;
+var b_nt;
 
 var mode_fire;
 var mode_satellite;
 var mode_meteorite;
 var mode_earthquake;
 var mode_clean;
-
+var mode_tc;
+var mode_od;
+var mode_dt;
+var mode_nt;
 
 function setup(){
 
@@ -66,13 +84,28 @@ function setup(){
     b_snow.mousePressed(mode_snow);
     flag_snow = 0;
 
+    b_tc = select("#true_color");
+    b_tc.mousePressed(mode_tc);
+    flag_tc = 0;
 
+    b_od = select("#prof_sea");
+    b_od.mousePressed(mode_od);
+    flag_od = 0;
+
+    b_dt = select("#day_temp");
+    b_dt.mousePressed(mode_dt);
+    flag_dt = 0;
+
+    b_nt = select("#night_temp");
+    b_nt.mousePressed(mode_nt);
+    flag_nt = 0;
+    
     b_clean = select("#clean");
     b_clean.mousePressed(mode_clean);
     flag_clean = 0;
 
     
-    canvas = createCanvas(windowWidth, windowWidth, WEBGL);
+    canvas = createCanvas(windowWidth, windowHeight, WEBGL);
     canvas.style('z-index', '-1');
     angleX = 0;
     angleY = 0;
@@ -101,6 +134,14 @@ function draw(){
 	texture(img_snow);
     } else if (flag_cloud){
 	texture(img_cloud);
+    } else if (flag_tc){
+	texture(img_tc);
+    } else if (flag_od){
+	texture(img_od);
+    } else if (flag_dt){
+	texture(img_dt);
+    } else if (flag_nt){
+	texture(img_nt);
     }
       sphere(r);
   pop();
@@ -187,11 +228,56 @@ function mode_sea(){
     }
 }
 
+var flag_tc;
+function mode_tc(){
+    if (flag_tc){
+	flag_tc = 0;
+    } else {
+	flag_tc = 1;
+    }
+}
+
+var flag_od;
+function mode_od(){
+    if (flag_od){
+	flag_od = 0;
+    } else {
+	flag_od = 1;
+    }
+}
+
+var flag_dt;
+function mode_dt(){
+    if (flag_dt){
+	flag_dt = 0;
+    } else {
+	flag_dt = 1;
+    }
+}
+
+var flag_nt;
+function mode_nt(){
+    if (flag_nt){
+	flag_nt = 0;
+    } else {
+	flag_nt = 1;
+    }
+}
+
+
+
 var flag_clean;
 function mode_clean(){
     flag_fire = 0;
     flag_earthquake = 0;
     flag_satellite = 0;
     flag_meteorite = 0;
+    flag_sea = 0;
+    flag_snow = 0;
+    flag_cloud = 0;
+    flag_tc = 0;
+    flag_od = 0;
+    flag_dt = 0;
+    flag_nt = 0;
 }
 
