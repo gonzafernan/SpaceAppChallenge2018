a8971706548622ef7ed6efec5ec5000feb0b2835
Tu Nombre <gonzaromero_2007@hotmail.com>
2018-10-21 10:59:26 -0300

    minor cahnges

--- globe3D/meteorite.js
+++ globe3D/meteorite.js
@@ -11,7 +11,7 @@ function displayMeteorite3D(){
     for (let i = 0; i<147; i++){
 	lon = meteor[i].geolocation.coordinates[0];
 	lat = meteor[i].geolocation.coordinates[1];
-  
+
   var theta = radians(lat);
   var phi = radians(lon) + PI/2;
   var x = r * cos(theta) * cos(phi);
@@ -22,6 +22,7 @@ function displayMeteorite3D(){
   fill(255);
   translate(x, y, z);
   box(5, 5, 5);
+  //sphere(10);
   pop();
     }
 }
